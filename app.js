936e356421511c7165a013cad421c325b97d9b0f
Milan J <milanjiji0987654321@gmail.com>
2024-09-02 15:16:07 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const textRoutes = require('./routes/textData');
+const fileRoutes = require('./routes/fileData');
+const states = require('./routes/statemanage');
+
+const SupperConnectNode = () => {
+    const app = express();
+
+    app.use(cors());
+    app.use(express.json());
+    app.use(bodyParser.json());
+
+    app.use('/api', textRoutes);
+    app.use('/api', fileRoutes);
+    app.use('/api', states);
+
+    app.get('/', (req, res) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.json({ message: 'Server Working' });
+    });
+
+    return app;
+};
+
+
+module.exports = SupperConnectNode;
